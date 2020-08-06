e651bc45fd854558b94ce94e7bc43a643a2e7e26
Jacobtco <jacobtco@gmail.com>
2020-08-06 13:43:41 +0800

    Removed the emit when connecting

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ let socket = io(); //creates our connection to the server (send and receive);
 socket.on('connect', function() {
   console.log('Server is connected'); // prints whenever the client connects to a server
 
-  socket.emit('createMessage', {
-    from: 'Jacob',
-    text: 'Wassup'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'Jacob',
+  //   text: 'Wassup'
+  // });
 });
 
 socket.on('disconnect', function() {
